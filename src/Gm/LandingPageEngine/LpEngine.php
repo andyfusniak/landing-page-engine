641b51d0bf5ad614643ff49649b084b43c8af1a9
codemagician <codemagician@greycatmedia.co.uk>
2016-11-28 17:56:31 +0000

    Add Twig template basic loading

--- src/Gm/LandingPageEngine/LpEngine.php
+++ src/Gm/LandingPageEngine/LpEngine.php
@@ -1,6 +1,39 @@
 <?php
 namespace Gm\LandingPageEngine;
 
+require_once 'vendor/twig/twig/lib/Twig/Autoloader.php';
+
 class LpEngine
 {
+    /**
+     * @var array
+     */
+    private $config;
+
+    /**
+     * @var \Twig_Environment
+     */
+    private $twigEnv;
+
+    public static function setup()
+    {
+    }
+
+    public function __construct(array $config)
+    {
+        $this->config = $config;
+        \Twig_Autoloader::register();
+        $loader = new \Twig_Loader_Filesystem($config['themes_root'] . '/' . $config['theme_name']);
+        $this->twigEnv = new \Twig_Environment($loader, [
+            'cache' => $config['cache_root'],
+        ]);
+    }
+
+    public function run()
+    {
+        $template = $this->twigEnv->load('html/template.html.twig');
+        echo $template->render([
+            'title' => 'Example title for our page'
+        ]);
+    }
 }
