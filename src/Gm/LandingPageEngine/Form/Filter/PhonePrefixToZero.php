bb6b85c9ab6c06b40b5e4eb1334218ce4037e4e2
codemagician <codemagician@greycatmedia.co.uk>
2016-12-04 12:59:21 +0000

    Add comment to phone prefix filter

--- src/Gm/LandingPageEngine/Form/Filter/PhonePrefixToZero.php
+++ src/Gm/LandingPageEngine/Form/Filter/PhonePrefixToZero.php
@@ -23,7 +23,8 @@ class PhonePrefixToZero extends AbstractFilter
         if (($stringLength >= 4)
             && (substr($value, 0, 1) === '+')) {
             
-            // if the telephone
+            // if the third digit is already a zero, then
+            // there is no need to add a 0 to the phone number
             if (substr($value, 3, 1) === '0') {
                 return substr($value, 4, $stringLength);
             } else {
