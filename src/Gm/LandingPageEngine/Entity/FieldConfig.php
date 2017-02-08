0f9f6efb736324bae9abf29fff6c0ce55ba09ea4
codemagician <codemagician@greycatmedia.co.uk>
2017-02-08 16:09:41 +0000

    <form optional="true"> attribute added

--- src/Gm/LandingPageEngine/Entity/FieldConfig.php
+++ src/Gm/LandingPageEngine/Entity/FieldConfig.php
@@ -23,6 +23,11 @@ class FieldConfig
     protected $dbColumn;
 
     /**
+     * @var bool
+     */
+    protected $optional;
+
+    /**
      * @var FilterConfigCollection
      */
     protected $filterConfigCollection;
@@ -32,32 +37,44 @@ class FieldConfig
      */
     protected $validatorConfigCollection;
 
-    public function __construct($name, $dbColumn)
+    public function __construct(string $name, string $dbColumn, bool $optional = false)
     {
         $this->name     = $name;
         $this->dbColumn = $dbColumn;
+        $this->optional = $optional;
     }
 
-    public function getName()
+    public function getName() : string
     {
         return $this->name;
     }
 
-    public function setName($name)
+    public function setName(string $name) : FieldConfig
     {
         $this->name = $name;
         return $this;
     }
 
-    public function getDbColumn()
+    public function getDbColumn() : string
     {
         return $this->dbColumn;
     }
 
-    public function setDbColumn($dbColumn)
+    public function setDbColumn(string $dbColumn) : FieldConfig
     {
         $this->dbColumn = $dbColumn;
         return $this;
+    }
+
+    public function setOptional(bool $optional) : FieldConfig
+    {
+        $this->optional = $optional;
+        return $this;
+    }
+
+    public function getOptional() : bool
+    {
+        return $this->optional;
     }
 
     public function setFilterConfigCollection(FilterConfigCollection $filterConfigCollection)
